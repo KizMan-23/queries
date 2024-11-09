88c37b1c4b8ad9d4225d543ec8f6c77a895427d7
KizMan-23 <kizitougwu777@gmail.com>
2024-11-09 23:40:51 +0100

    new dataset

--- employees_query.sql
+++ employees_query.sql
@@ -1,3 +1,11 @@
 USE company_db
 
+'Q1 -- FIND THE EMPLOYEE WHO''S SALARY IS MORE THAN THE AVERAGE SALARY OF ALL EMPLOYEE'
+
+SELECT * FROM employees
+JOIN salaries sa on sa.emp_no = employees.emp_no  /* inconsistency of emp_no between employees' table and other tables */ 
+WHERE salary > (
+	SELECT AVG(salary) FROM salaries);  -- 53760.4779
+
+'Q2 -- 
 
