767aefaea811a4e10837f80b6fde58573c8870ee
KizMan-23 <kizitougwu777@gmail.com>
2024-11-11 23:13:22 +0100

    final workings

--- employees_query.sql
+++ employees_query.sql
@@ -51,7 +51,6 @@ select * from sample_employees s
 where not exists (select * from dept_managers d where s.department = d.department);
                         
 'CORRELATED SUBQUERY'
-
 'Q4 -- FIND EMPLOYEES IN EACH DEPARTMENT WHO EARN MORE THAN THE AVERAGE SALARY IN THAT DEPARTMENT'
 
 select department, avg(salary)
@@ -63,3 +62,48 @@ where salary > ( select avg(salary)
 				from sample_employees e2
 				where e2.department = e1.department);
                 
+select * from sample_employees e1
+join sample_employees e2 on e2.emp_no = e1.emp_no
+where e1.salary > ( select avg(salary)
+				from sample_employees e2
+				where e2.department = e1.department);
+                
+
+'NESTED QUERY'
+'Q5 -- FETCH ALL EMPLOYEE DETAILS AND ADD REMARKS TO THOSE WHO EARN MORE THAN THE AVERAGE EMPLOYEE'
+
+SELECT * ,
+(CASE WHEN salary > (select Avg(salary) from sample_employees)
+	  then 'Higher Salary than Avg'
+	else "Below Avg"
+            end ) AS remarks
+FROM sample_employees
+
+------2 
+SELECT * ,
+(CASE WHEN salary > avg_sal.sal
+	then 'Higher Salary than Avg'
+	else "Below Avg"
+	end ) AS remarks
+FROM sample_employees
+cross join (select Avg(salary) sal from sample_employees) avg_sal
+
+'Q6 -- Insert data to employee history table. Make sure not to insert duplicate records'
+
+INSERT INTO employee_history
+SELECT distinct se.emp_no, se.first_name, se.last_name, se.emp_status, se.salary
+from sample_employees se
+where not exists ( select 1 from employee_history eh
+					where eh.emp_no = se.emp_no);
+
+
+'Q7 - Gice 10% increment to all employees of Staff and Senior Staff position based on the maximum salary earned by an emp in each deot'
+
+with dept_max as (select department, max(salary) as top_salary from sample_employees
+group by department)
+
+select *, (top_salary + (top_salary*0.1)) as bonus 
+from sample_employees e1
+join dept_max dm ON dm.department = e1.department
+where e1.emp_status = ("Staff" or 'Senior Staff') 'Use of OR in query'
+
--- table_values.sql
+++ table_values.sql
@@ -8,7 +8,7 @@ VALUES 	(101, 'Mohan', 40000),
         (105, 'Alice', 80000),
         (106, 'Leremy', 90000);
         
-INSERT INTO cte_trial.sales (store_id, store_name, product, quantity, cost)
+INSERT INTO cte_trial.sales(store_id, store_name, product, quantity, cost)
 VALUES 	(1, 'Apple Originals 1', 'iPhone 12 Pro', 1, 1000),
 		(1, 'Apple Originals 1', 'MacBook pro 13', 3, 2000),
         (1, 'Apple Originals 1', ' AirPods Pro', 2, 200),
@@ -20,4 +20,18 @@ VALUES 	(1, 'Apple Originals 1', 'iPhone 12 Pro', 1, 1000),
         (3, 'Apple Originals 3', 'AirPods Pro', 3, 280),
         (4, 'Apple Originals 4', 'iPhone 12 Pro', 2,1000),
         (4, 'Apple Originals 4', ' MacBook Pro 13', 1, 2500);
-        
+	
+    
+INSERT INTO sales VALUES 
+(1, 'Apple Originals 1', 'iPhone 12 Pro', 1, 1000),
+(1, 'Apple Originals 1', 'MacBook pro 13', 3, 2000),
+(1, 'Apple Originals 1', ' AirPods Pro', 2, 200),
+(2, 'Apple Originals 2', 'iPhone 12 Pro', 2, 1000),
+(3, 'Apple Originals 3', 'iPhone 12 Pro', 1,1000),
+(3, 'Apple Originals 3', 'MacBook pro 13', 1, 2000),
+(3, 'Apple Originals 3', 'MackBook Air', 4, 1100),
+(3, 'Apple Originals 3', 'iPhone 12', 2, 1000),
+(3, 'Apple Originals 3', 'AirPods Pro', 3, 280),
+(4, 'Apple Originals 4', 'iPhone 12 Pro', 2,1000),
+(4, 'Apple Originals 4', ' MacBook Pro 13', 1, 2500);
+
